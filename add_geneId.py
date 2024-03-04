cc910b813d51986e9f9dfd21f66f3a1d42ba894d
Michael Van de Voorde <71884935+MMichaelVdV@users.noreply.github.com>
2024-03-04 21:43:15 +0100

    Add files via upload

---
+++ add_geneId.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 21:19:21 2024
+"""
+
+from Bio import SeqIO
+
+original_file = "Lotus_japonicus.fa"
+corrected_file = "Lotus_japonicus_corrected.fa"
+
+with open(original_file) as original, open(corrected_file, 'w') as corrected:
+    records = SeqIO.parse(original_file, 'fasta')
+    for record in records:
+        print(record.id)         
+        geneId = (record.id).split('.')[0]
+        record.description = "gene:" + str(geneId)
+        print(record.id) 
+        SeqIO.write(record, corrected, 'fasta')
